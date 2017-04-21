f0d9eedd7dc0f5b5d282a09633b32fd33b34dd88
minjung kim <swatmj@gmail.com>
2017-04-21 16:59:16 +0900

    -서비스 UI 구현중

--- src/hotelclient/resv/ResvPanel.java
+++ src/hotelclient/resv/ResvPanel.java
@@ -1,22 +1,27 @@
 package hotelclient.resv;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.sql.Connection;
+import java.util.Calendar;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import hotelclient.ClientMain;
 
 public class ResvPanel extends JPanel{
 	ClientMain main;
 	Connection con;
-	
-	
+
 	public ResvPanel(ClientMain main) {
 		this.main=main;
 		con=main.con;
-	
+
 		setBackground(Color.GRAY);
 		setPreferredSize(new Dimension(1100, 900));
 		setVisible(false);	
