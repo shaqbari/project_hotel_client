4ab4ecf65291fd3fb487a1e87e3ae1515cccc1e8
MinjungKim-PC\Minjung Kim <swatmj@gmail.com>
2017-04-22 20:56:01 +0900

    -서비스주문 작업중

--- src/hotelclient/service/Serv_view.java
+++ src/hotelclient/service/Serv_view.java
@@ -2,9 +2,14 @@ package hotelclient.service;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -13,17 +18,21 @@ import javax.swing.JPanel;
 
 import hotelclient.ClientMain;
 
-public class Serv_view extends JPanel implements ActionListener{
-	ServicePanel sp;
+public class Serv_view extends JPanel{
+	Serv_Detail sd;
+	Connection con;
 	ClientMain main;
 	JButton bt;
 	ImageIcon icon;
 	JLabel la;
 	URL url;
+	ArrayList<Service> service = new ArrayList<Service>();
 	
 	public Serv_view(ClientMain main, JButton bt, URL url, String serv_name) {
 		this.main = main;
 		this.bt = bt;
+		con=main.con;
+		
 		icon = new ImageIcon(url);
 		la = new JLabel(serv_name);
 		
@@ -38,19 +47,16 @@ public class Serv_view extends JPanel implements ActionListener{
 		bt.setFocusPainted(false);
 		bt.setOpaque(false);
 
-		bt.addActionListener(this);
-
 		add(bt);
 		add(la);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		System.out.println(bt.getName());
-		/*Object obj=e.getSource();
-		if(obj==){
-			System.out.println(0+"입니다");
-		}*/
-	}
-
+		
+		setVisible(true);
+		
+		bt.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				//System.out.println("새로운 창 등장해야함");
+				sd = new Serv_Detail(main,serv_name,icon);
+			}
+		});	
+	}	
 }
--- src/hotelclient/service/ServicePanel.java
+++ src/hotelclient/service/ServicePanel.java
@@ -6,38 +6,46 @@ import java.awt.FlowLayout;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import hotelclient.ClientMain;
 
 public class ServicePanel extends JPanel {
 	ClientMain main;
 	Connection con;
+	//JTextArea txt;
+	//Vector<String> columnName = new Vector<String>();
+	//Vector<Vector> list = new Vector<Vector>();
 	
-	String[] serv_name = {"beer","breakfast","dinner","lunch","snack","wine"};
-	
+	String[] serv_name={"breakfast","lunch","dinner","beer","wine","snack"};
+			
 	JButton bt_beer,bt_breakfast,bt_dinner,bt_lunch,bt_snack,bt_wine;
 	JButton[] buttons = new JButton[6];
-	Serv_view[] serv_view= new Serv_view[serv_name.length];
+	Serv_view[] serv_view= new Serv_view[buttons.length];
 	
-	JLabel label;
-	ImageIcon imae;
+	//JLabel label;
+	//ImageIcon img;
 	
 	public ServicePanel(ClientMain main) {
 		setLayout(new FlowLayout());
 		this.main=main;
 		con=main.con;
+		//txt = new JTextArea(20,30);
+		//getList();
 		
-		buttons[0]=bt_beer=new JButton();
-		buttons[1]=bt_breakfast=new JButton();
+		buttons[0]=bt_breakfast=new JButton();
+		buttons[1]=bt_lunch=new JButton();
 		buttons[2]=bt_dinner=new JButton();
-		buttons[3]=bt_lunch=new JButton();
-		buttons[4]=bt_snack=new JButton();
-		buttons[5]=bt_wine=new JButton();
+		buttons[3]=bt_beer=new JButton();
+		buttons[4]=bt_wine=new JButton();
+		buttons[5]=bt_snack=new JButton();
 		
 		for(int i=0; i<serv_name.length; i++){
 			URL url = null;
@@ -49,10 +57,14 @@ public class ServicePanel extends JPanel {
 			serv_view[i] = new Serv_view(main, buttons[i], url, serv_name[i]);
 			add(serv_view[i]);
 		}
-	
+		
+		//add(txt);
 		
 		setBackground(Color.DARK_GRAY);
 		setPreferredSize(new Dimension(1100, 900));
 		setVisible(false);	
-	}
+	
+	}//»ý¼ºÀÚ
+	
+
 }
