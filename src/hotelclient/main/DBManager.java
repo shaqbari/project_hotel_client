3a7068d0d63068a58e4b5e34937ef14866060244
SIST108\user <shaqbari@gmail.com>
2017-04-25 14:43:57 +0900

    db설정 dbconfig폴더의 xml파일에 기재

--- src/hotelclient/main/DBManager.java
+++ src/hotelclient/main/DBManager.java
@@ -11,14 +11,17 @@ package hotelclient.main;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class DBManager { //여기서 class는 클래스 생성하는 예약어
 //이파일의 확장자를java로바꾸고db정보입력하세요
 	static private DBManager instance;
+	XMLParser parser=new XMLParser();	
+	Map<String, String> dbConfig=parser.dbHandler.dbConfig;
 	private String driver="oracle.jdbc.driver.OracleDriver";
-	private String url="jdbc:oracle:thin:@211.238.142.108:1521:XE";//@다음에 공유폴더db접속 ip입력하세요
-	private String user="hotelmanager";
-	private String password="manager4";
+	private String url="jdbc:oracle:thin:@"+dbConfig.get("ip")+":"+dbConfig.get("port")+":"+dbConfig.get("sid"); //@다음공유폴더db접속 ip입력하세요
+	private String user=dbConfig.get("user");//빈칸에 username입력하세요
+	private String password=dbConfig.get("password");//빈칸에 비밀번호입력하세요
 	
 	private Connection con;
 	
