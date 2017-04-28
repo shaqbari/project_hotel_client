5755ebc207a0fa20a1bfa0e56240beb7b5af5e81
SIST108\user <shaqbari@gmail.com>
2017-04-28 11:34:07 +0900

    회원로그인 버그 수정

--- src/hotelclient/network/MemberLoginReact.java
+++ src/hotelclient/network/MemberLoginReact.java
@@ -30,6 +30,7 @@ public class MemberLoginReact {
 	String member_name;
 	String resv_id;
 	String resv_time;
+	String end_time;
 	int stay;
 	
 	String yyyy;
@@ -42,7 +43,10 @@ public class MemberLoginReact {
 			"result":"yes",
 			"hotel_user_id":1,
 			"member_name":"김민정",
+			
+			//아래는 예약했을때만 보낸다
 			"resv_time":"2017-04-17-18-19-23",
+			"end_time":"2017-04-17-18-19-23",
 			"stay:1		
 		}*/
 	
@@ -53,15 +57,14 @@ public class MemberLoginReact {
 		this.checkUserPanel=main.checkAdminPanel;
 		this.homePanel=main.homePanel;
 		this.json=json;
-		
-		
 		result=json.get("result").toString();
 		if (result.equalsIgnoreCase("yes")) {
 			hotel_user_id=Integer.parseInt(json.get("hotel_user_id").toString());
 			member_name=json.get("member_name").toString();
-			if (json.get(resv_id)!=null) {
+			if (json.get("resv_id")!=null) {
 				resv_id = json.get("resv_id").toString();
 				resv_time = json.get("resv_time").toString();
+				end_time=json.get("end_time").toString();
 				stay = Integer.parseInt(json.get("stay").toString());
 			}			
 		}
@@ -94,14 +97,14 @@ public class MemberLoginReact {
 			main.la_user.setText(member_name);	
 			main.hotel_user_id=hotel_user_id;
 			
-			if (json.get(resv_id)!=null) {
+			if (resv_id!=null) {
 				dateProcess(resv_time);
 				homePanel.getResv_id_input().setText(resv_id);
 				homePanel.getGuest_name_input().setText(member_name);
 				homePanel.getResv_time_input().setText(resv_time);
 				//stay가아니라 종료날짜를 따로 db에 입력하자.
 				homePanel.getStay_input()
-						.setText(yyyy + "-" + mm + "-" + Integer.toString((Integer.parseInt(dd) + stay)));
+						.setText(end_time);
 			}
 			
 			reset();
